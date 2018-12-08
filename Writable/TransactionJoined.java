ba93195c61303e310869e34b7b26024e3e90552b
Andria Njoku <andrianjoku@gmail.com>
2018-12-08 11:15:40 +0000

    Tried to add read and write methods for serialization but was having too many errors so have made some simplifications. Will revisit after submission.

--- Writable/TransactionJoined.java
+++ Writable/TransactionJoined.java
@@ -9,73 +9,81 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 
-public class TransactionJoined implements Writable, WritableComparable<TransactionJoined>{
+public class TransactionJoined  {
 	
 
 	//Will contain all fields of transactionout and transaction in joined together
 	//This transaction hash was used to join the two data sets and as such i will emit one version 
 	
-    public Text hash = new Text();
-	
+    private Text hash;
 	//this is equal to the vout field, if the coins have been respent
-	public Text txid = new Text();
-
-	
+	private Text txid;	
 	//If these two fields are equal then the money from the transaction has been respecnt. This is highly likely
-	public Text n = new Text();
+	private Text n;
 	
-	public Text vout = new Text();
-	
+	private Text vout;
 	//Value will be used to sort obviously
-	public IntWritable value = new IntWritable();
-
+	private IntWritable value;
 	
+	public TransactionJoined(){
+		
+		hash= new Text();
+		txid=new Text();
+		n=new Text();
+		vout=new Text();
+		value=new IntWritable();
 	
-	public void Join( TransactionOutWritable out, TransactionInWriatable in)
-	{
-		
-		hash.set(out.hash);
-		txid.set(in.txid);
-		n.set(out.n);
-		vout.set(in.vout_magnet);
-		value=out.value;
-		
-		
-		
 	}
 
-	@Override
-	public int compareTo(TransactionJoined o) {
+
+	public Text getHash() {
+		// TODO Auto-generated method stub
+		return hash;
+	}
+
+
+
+	public Text getId() {
+		// TODO Auto-generated method stub
+		return txid;
+	}
+
+
+
+	public Text getVout() {
+		// TODO Auto-generated method stub
+		return vout;
+	}
+
+
+
+	public Text getN() {
+		// TODO Auto-generated method stub
+		return n;
+	}
+
+
+
+	public IntWritable getBtc() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+
+
+
+	public void inflate(String shash, String id, String n, String vout, String btc) {
 		// TODO Auto-generated method stub
 		
-		int res=0;
-	       
-		res = this.value.compareTo(o.value);
 		
-	    return res;
-	}
+		hash.set(shash);
+		txid.set(id);
+		this.n.set(n);
+		this.vout.set(vout);
+		int vo = Integer.parseInt(btc);
+		value.set(vo);
 
-	@Override
-	public void readFields(DataInput arg0) throws IOException {
-		// TODO Auto-generated method stub
 		
 	}
+     }
 
-	@Override
-	public void write(DataOutput arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public String toString() {
-		
-		// we dont need hash as its going to be in the first column 
-		
-		return " ID:" +txid+", N:"+n+", Vout:"+vout+ ", Bitcoin:" + value;
-		
-		
-		
-	}
 
-}
